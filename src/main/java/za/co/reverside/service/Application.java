ecef141d7d11b3db2fab902d7b67e5d0a1234ae4
Manmay <manmay.e.mohanty@gmail.com>
2017-04-09 22:43:54 +0200

    Hello

--- src/main/java/za/co/reverside/service/Application.java
+++ src/main/java/za/co/reverside/service/Application.java
@@ -1,14 +1,14 @@
 package za.co.reverside.service;
 
-import com.zenerick.service.DocumentService;
 
 import com.mangofactory.swagger.plugin.EnableSwagger;
 
+import com.zenerick.service.document.Service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Service
 @EnableSwagger
-@DocumentService
 @SpringBootApplication
 public class Application {
 
