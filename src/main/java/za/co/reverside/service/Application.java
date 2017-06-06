813ba538c6ca3e70e95c87071ab3485edd812642
f4789334 <manmay.e.mohanty@gmail.com>
2017-06-06 17:28:05 +0200

    Implementing with ES module

--- src/main/java/za/co/reverside/service/Application.java
+++ src/main/java/za/co/reverside/service/Application.java
@@ -1,14 +1,11 @@
 package za.co.reverside.service;
 
-import com.mangofactory.swagger.plugin.EnableSwagger;
 import com.zenerick.service.document.Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Service
-@EnableSwagger
-@SpringBootApplication
 public class Application {
 
     public static void main(String[] args){
